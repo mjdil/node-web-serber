c1a744fe3ecdb4f88a5ed4b6ceb1b84e21105805
Muhammad Jamal <muhammadjamal@Muhammads-MacBook-Pro.local>
2018-02-12 19:10:58 -0500

    fixed formatting

--- server.js
+++ server.js
@@ -1,44 +1,62 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 const port = process.env.PORT || 3000;
-
 var app = express();
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   var now = new Date().toString();
-  console.log(`${now}, ${req.method}, ${req.url}`);
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
 });
 
-//app.use((req, res, next)=> {
-//  res.render('maintenance.hbs');
-//});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
-hbs.registerHelper('getCurrentYear', ()=>{
-  return new Date().getFullYear()
-})
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'home page',
-    welcomeMessage: 'welcome to my website',
-    currentYear: new Date().getFullYear()
-  })
-});
-
-app.get('/about', (req, res)=> {
-  res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
-app.get('/bad', (req, res) => {
-  res.send('error');
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
-app.listen(port);
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
